a3715872b5cb5a3e87f52c2c5c63c5196fe59f4d
Kartik-Ranaut <ranautk6@gmail.com>
2024-03-28 17:02:37 +0530

    added backend api files

--- Database.js
+++ Database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 exports.dbconnect =()=>{
     mongoose.connect(process.env.Database_url,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
     }).then(()=>{console.log('congratulations your database is connected and the database name is todoproject')})
     .catch((error)=>{console.log(error)});
 }
